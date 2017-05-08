ef4a26c5621d0c82740f4c56f089e2fb53989224
Sergi Garcia <sergigarciapena@gmail.com>
2017-05-08 20:58:57 +0200

    show routeTrack in google map; googleKey hidden

---
+++ routes/APIroutes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const APIroutes = express.Router();
+const multer = require('multer');
+var uploadPhoto = multer({
+  dest: "./public/uploads/albumns"
+});
+var uploadTrack = multer({
+  dest: "./public/uploads/tracks"
+});
+const Picture = require("../models/picture");
+
+// User model
+const User = require("../models/user");
+const Wall = require("../models/wall");
+const Route = require("../models/route");
+const Message = require("../models/message");
+const Albumn = require("../models/albumn");
+const Track = require("../models/track");
+
+const passport = require("passport");
+
+const auth = require('../helpers/auth-helpers');
+
+ //auth.ensureLoggedIn('/login'),
+APIroutes.get('/:route_id/trackname', (req, res, next) => {
+  Route.findById({_id:req.params.route_id}).populate("track").exec((err, route) => {
+    if (err) {
+      res.status(500).json({message: err});
+    } else {
+      console.log("hi",route.track.file_path);
+      res.status(200).json(route.track.file_path);
+    }
+  });
+
+});
+
+module.exports= APIroutes;
